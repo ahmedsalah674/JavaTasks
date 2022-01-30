392d24b263e5c7cbcd9940d30ba7c6173dd2d83b
ahmedsalah674 <ahmad.mohamadsalah@gmail.com>
2022-01-30 11:28:21 +0200

    open and save option and shourtcuts

--- Notepad/src/Notepad.java
+++ Notepad/src/Notepad.java
@@ -4,6 +4,9 @@
  */
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
@@ -18,7 +21,9 @@ import javafx.stage.Stage;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.SeparatorMenuItem;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.FileChooser;
 
@@ -27,116 +32,136 @@ import javafx.stage.FileChooser;
  * @author ahmedsalah
  */
 public class Notepad extends Application {
-
+    
     @Override
     public void start(Stage primaryStage) {
-
+        
         MenuBar manubar = new MenuBar();
         Menu fileMenu = new Menu("file");
         Menu editMenu = new Menu("Edit");
         Menu helpMenu = new Menu("Help");
-
+        
         MenuItem newOption = new MenuItem("New");
+        newOption.setAccelerator(KeyCombination.keyCombination("Ctrl+n"));
         MenuItem openOption = new MenuItem("Open");
+        openOption.setAccelerator(KeyCombination.keyCombination("Ctrl+o"));
         MenuItem saveOption = new MenuItem("Save");
+        saveOption.setAccelerator(KeyCombination.keyCombination("Ctrl+s"));
         MenuItem exitOption = new MenuItem("Exit");
-
+        exitOption.setAccelerator(KeyCombination.keyCombination("Alt+F4"));
+        
         MenuItem undoOption = new MenuItem("Undo");
+        undoOption.setAccelerator(KeyCombination.keyCombination("Ctrl+z"));
         MenuItem cutOption = new MenuItem("Cut");
+        cutOption.setAccelerator(KeyCombination.keyCombination("Ctrl+x"));
         MenuItem copyOption = new MenuItem("Copy");
+        copyOption.setAccelerator(KeyCombination.keyCombination("Ctrl+c"));
         MenuItem pastOption = new MenuItem("Past");
+        pastOption.setAccelerator(KeyCombination.keyCombination("Ctrl+v"));
         MenuItem deleteOption = new MenuItem("Delete");
+        deleteOption.setAccelerator(KeyCombination.keyCombination("Ctrl+d"));
         MenuItem selectAllOption = new MenuItem("Select All");
-
+        selectAllOption.setAccelerator(KeyCombination.keyCombination("Ctrl+a"));
+        
         MenuItem aboutOption = new MenuItem("About Notepad");
-
-        fileMenu.getItems().addAll(newOption, openOption, saveOption, exitOption);
-        editMenu.getItems().addAll(undoOption, cutOption, copyOption, pastOption, deleteOption, selectAllOption);
+        aboutOption.setAccelerator(KeyCombination.keyCombination("Ctrl+i"));
+        SeparatorMenuItem sep = new SeparatorMenuItem();
+        SeparatorMenuItem sep2 = new SeparatorMenuItem();
+        
+        fileMenu.getItems().addAll(newOption, openOption, saveOption, sep, exitOption);
+        editMenu.getItems().addAll(undoOption, cutOption, copyOption, pastOption, deleteOption, sep2, selectAllOption);
         helpMenu.getItems().addAll(aboutOption);
-
+        
         manubar.getMenus().addAll(fileMenu, editMenu, helpMenu);
-
+        
         TextArea txtEditor = new TextArea();
-
+        
         BorderPane pane = new BorderPane();
         pane.setTop(manubar);
         pane.setCenter(txtEditor);
-
+        
         Scene s = new Scene(pane, 300, 400);
         txtEditor.setMaxWidth(s.getWidth());
         txtEditor.setMaxHeight(s.getHeight());
-
+        
         Stage st = new Stage();
         st.setScene(s);
         st.show();
-
+        
         newOption.addEventHandler(ActionEvent.ACTION, (ActionEvent event) -> {
             txtEditor.clear();
         });
-
+        
         saveOption.setOnAction((ActionEvent event) -> {
             FileChooser fc = new FileChooser();
-            File saveFile = fc.showSaveDialog(null);
+            File savefile = fc.showSaveDialog(null);
             try {
-                Dialog<String> dialog = new Dialog<String>();
-                dialog.setTitle("save path");
-                ButtonType type = new ButtonType("Ok", ButtonData.OK_DONE);
-                dialog.setContentText(saveFile.toString());
-                dialog.getDialogPane().getButtonTypes().add(type);
-                dialog.showAndWait();
-            } catch (Exception e) {
-                System.out.println(e);
+                FileWriter fw = new FileWriter(savefile);
+                fw.write(txtEditor.getText());
+                fw.close();
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage());
             }
         });
-
+        
         exitOption.addEventHandler(ActionEvent.ACTION, (ActionEvent event) -> {
             Platform.exit();
         });
-
+        
         undoOption.addEventHandler(ActionEvent.ACTION, (ActionEvent event) -> {
             txtEditor.undo();
         });
-
+        
         cutOption.addEventHandler(ActionEvent.ACTION, (ActionEvent event) -> {
             txtEditor.cut();
         });
-
+        
         copyOption.addEventHandler(ActionEvent.ACTION, (ActionEvent event) -> {
             txtEditor.copy();
         });
-
+        
         pastOption.addEventHandler(ActionEvent.ACTION, (ActionEvent event) -> {
             txtEditor.paste();
         });
-
+        
         deleteOption.addEventHandler(ActionEvent.ACTION, (ActionEvent event) -> {
             IndexRange selection = txtEditor.getSelection();
             int start = selection.getStart();
             int end = selection.getEnd();
             txtEditor.deleteText(start, end);
         });
-
+        
         selectAllOption.addEventHandler(ActionEvent.ACTION, (ActionEvent event) -> {
             txtEditor.selectAll();
         });
-
+        
         openOption.addEventHandler(ActionEvent.ACTION, (ActionEvent event) -> {
             FileChooser fc = new FileChooser();
             File openFile = fc.showOpenDialog(st);
-            try {
-                Dialog<String> dialog = new Dialog<String>();
-                dialog.setTitle("open path");
-                ButtonType type = new ButtonType("Ok", ButtonData.OK_DONE);
-                dialog.setContentText(openFile.toString());
-                dialog.getDialogPane().getButtonTypes().add(type);
-                dialog.showAndWait();
-            } catch (Exception e) {
-                System.out.println(e);
+            if (openFile != null) {
+                FileInputStream fis = null;
+                int size;
+                byte[] byto;
+                try {
+                    fis = new FileInputStream(openFile);
+                    size = fis.available();
+                    byto = new byte[size];
+                    fis.read(byto);
+                    txtEditor.setText(new String(byto));
+                } catch (IOException ex) {
+                    System.out.println(ex.getMessage());
+                } finally {
+                    try {
+                        fis.close();
+                    } catch (IOException ex) {
+                        System.out.println(ex.getMessage());
+                    }
+                }
             }
-
+            
         });
         aboutOption.setOnAction((ActionEvent event) -> {
-
+            
             Dialog<String> dialog = new Dialog<String>();
             dialog.setTitle("about");
             ButtonType type = new ButtonType("Ok", ButtonData.OK_DONE);
@@ -144,7 +169,7 @@ public class Notepad extends Application {
             dialog.getDialogPane().getButtonTypes().add(type);
             dialog.show();
         });
-
+        
     }
 
     /**
@@ -153,5 +178,5 @@ public class Notepad extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-
+    
 }
